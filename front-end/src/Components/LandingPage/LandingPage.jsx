c5686fa54fea90e545f298e40fe241ecf5b08fc6
samcodedev <obanlasamuelolakunle@gmail.com>
2023-11-03 04:06:25 +0100

    weather_integration

--- front-end/src/Components/LandingPage/LandingPage.jsx
+++ front-end/src/Components/LandingPage/LandingPage.jsx
@@ -38,9 +38,24 @@ const LandingPage = () => {
     console.log(result);
   }
 
+  
+const weather = async () =>{
+  let result = await fetch(
+    "http://api.weatherapi.com/v1/current.json?key=ad064c1cf6b14c5aada113203230111&q=nigeria&aqi=yes",
+    {
+      method: 'get'
+    }
+  )
+  result = await result.json()
+  localStorage.setItem('weather', JSON.stringify(result))
+  console.log(result);
+}
+
+
   useEffect(()=>{
     handleListedStock()
     handleStock()
+    weather()
   },[])
 
   return (
